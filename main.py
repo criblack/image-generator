8829aa12c5545c92f2143eee3cbc2336a56bb50f
Vitor Daniel <vitor036daniel@gmail.com>
2023-01-06 12:02:12 -0300

    update labellllllllç

--- main.py
+++ main.py
@@ -21,20 +21,19 @@ app.add_middleware(
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+
 @app.post("/")
 def generate_file(image: UploadFile, description: str = Form()):
     image_bytes = io.BytesIO()
     new_image = Image.open(image.file)
+    if new_image.size[0] != new_image.size[1]:
+        new_image = new_image.resize((new_image.size[0], new_image.size[0]))
+
     new_image.save(image_bytes, format="PNG")
 
-    mask = Image.new("L", new_image.size, 0)
-    mask_bytes = io.BytesIO()
-    mask.save(mask_bytes, format="PNG")
-
-    response = openai.Image.create_edit(
+    response = openai.Image.create_variation(
         image=image_bytes.getvalue(),
-        mask=mask_bytes.getvalue(),
         prompt=description
     )
 
-    return response["data"][0]["url"]
+    return [image["url"] for image in response["data"]]
